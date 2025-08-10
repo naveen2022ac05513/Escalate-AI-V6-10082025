4bf3adf8b0386a96a7ff5558f24f136b2674e66e
naveen2022ac05513 <2022ac05513@wilp.bits-pilani.ac.in>
2025-08-10 20:39:00 +0530

    Update EscalateAIV610082025.py

--- EscalateAIV610082025.py
+++ EscalateAIV610082025.py
@@ -21,7 +21,7 @@ from sklearn.model_selection import train_test_split
 import threading
 from dotenv import load_dotenv
 
-from escalateai_enhancements import (
+from EscalateAIV610082025 Enhancements import (
     extract_tags, show_feature_importance, train_model,
     escalation_trend_chart, generate_pdf_report,
     enable_dark_mode, sticky_filter_summary
