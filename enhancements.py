09a7c7760b1a7bbb063f16aa58941078ae80635e
naveen2022ac05513 <2022ac05513@wilp.bits-pilani.ac.in>
2025-08-11 19:45:15 +0530

    Update enhancements.py

--- enhancements.py
+++ enhancements.py
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 import sqlite3
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import plotly.io as pio
+pio.kaleido.scope.default_format = "png"
 
 DB_PATH = "escalations.db"
 
